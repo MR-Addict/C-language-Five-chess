d82618c18d31555f0c2bc8c8d2c77555342b7ea5
OwenCael <75357598+MR-Addict@users.noreply.github.com>
2021-08-24 19:06:36 +0800

    Create printpattern.c

---
+++ Code/printpattern.c
@@ -0,0 +1,17 @@
+//打印棋盘函数，参数是定义的数组
+void printPattern(int a,int b,int c,int d,int e,int f,int g,int h,int i)
+{
+	printf("************************\n");
+	printf("*       *       *      *\n");
+    printf("*   %c   *   %c   *   %c  *\n",a,b,c);
+	printf("*       *       *      *\n");
+    printf("************************\n");
+	printf("*       *       *      *\n");
+    printf("*   %c   *   %c   *   %c  *\n",d,e,f);
+	printf("*       *       *      *\n");
+	printf("************************\n");
+    printf("*       *       *      *\n");
+    printf("*   %c   *   %c   *   %c  *\n",g,h,i);
+    printf("*       *       *      *\n");
+	printf("************************\n");
+}
